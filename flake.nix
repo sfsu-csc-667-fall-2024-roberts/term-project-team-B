c833131a6aad637d66ada857ceac63e99da42ed8
Uzair Mohammed <uzair.hamed@gmail.com>
2024-12-18 21:05:07 -0800

    Formatted Nix flake

--- flake.nix
+++ flake.nix
@@ -3,23 +3,39 @@
 
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.default ]; };
-      });
-    in
-    {
-      overlays.default = final: prev: rec {
-        nodejs = prev.nodejs;
-        yarn = (prev.yarn.override { inherit nodejs; });
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+      supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+      config = {
+        allowUnfree = true;
       };
-
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [ node2nix nodejs nodePackages.pnpm yarn nodePackages_latest.typescript-language-server ];
-        };
-      });
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [self.overlays.default];
+          };
+        });
+  in {
+    overlays.default = final: prev: rec {
+      nodejs = prev.nodejs;
+      yarn = prev.yarn.override {inherit nodejs;};
     };
+
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          node2nix
+          nodejs
+          nodePackages.pnpm
+          yarn
+            nodePackages_latest.typescript-language-server
+            mongodb
+        ];
+      };
+    });
+  };
 }
